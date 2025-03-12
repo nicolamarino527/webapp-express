e16d4718489509fbc1a60a6025642186677decda
nicolamarino <nicolamarino527@gmail.com>
2025-03-12 21:02:37 +0100

    [feat] add app uses

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // express
 const express = require('express');
 const app = express();
-const port = process.env.PORT;
+const port = 3000;
 
 // importiamo cors
 const cors = require('cors')
@@ -16,4 +16,32 @@ const errorsHandler = require("./middlewares/errorsHandler");
 const imagePathMiddleware = require('./middlewares/imagePath');
 const notFound = require("./middlewares/notFound");
 
-// configuriamo il body parser
+
+// cartella per i file statici
+app.use(express.static('public')); s
+
+// middleware cors
+app.use(cors({ origin: "http://localhost:5173" }))
+
+
+// body-parser
+app.use(express.json());
+
+// rotta home
+app.get('/api', (req, res) => {
+    res.send("rotta home");
+})
+
+// middleware path 
+app.use(imagePathMiddleware);
+
+// gestione errore
+app.use(errorsHandler);
+
+// utilizzo not found
+app.use(notFound);
+
+// avvio del server sulla porta specificata
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
--- middlewares/imagePath.js
+++ middlewares/imagePath.js
@@ -1,6 +1,6 @@
 function setImagePath(req, res, next) {
     // creaimo il path assoluto della img
-    req.imagePath = `${req.protocol}://${req.get('host')}/img/books/`;
+    req.imagePath = `${req.protocol}://${req.get('host')}/img//`;
     next()
 }
 
